faa9d96860c1077ae129fd59975d902f25032174
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-07-14 11:40:49 -0700

    Add Paperclip for product image upload.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   has_many :order_items
   validates :name, :description, :price, presence: true
   validates_numericality_of :price, greater_than: 0
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
