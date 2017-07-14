336766f7838f34a7e058ea4793fc80259c94a178
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-07-14 09:33:17 -0700

    Add AJAX to delete items from cart, update total price and total items count

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -20,10 +20,13 @@ class OrderItemsController < ApplicationController
 
   def destroy
     @order = current_order
-    @item = @order.order_items.find(params[:id])
+    @item = @order.order_items.find(params[:id])    
     @item.destroy
     @order.save
-    redirect_to cart_path
+    respond_to do |format|
+      format.html { redirect_to cart_path}
+      format.js
+    end
   end
 
   private
