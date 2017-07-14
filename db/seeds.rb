333c7bea2a6398f9a4ebaa86a47f5045829646a0
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-07-14 12:52:51 -0700

    Add product show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,12 @@ class Seed
 
   def generate_products
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop, image: open("public/images/test.jpeg"))
     end
   end
 end
 
+Product.destroy_all
+OrderItem.destroy_all
+Order.destroy_all
 Seed.begin
