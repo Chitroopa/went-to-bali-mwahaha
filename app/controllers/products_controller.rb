6ff4b20117976ee9eb036b59d2069732fe09f575
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-07-14 13:46:01 -0700

    Add product edit functionality to admin

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,6 +36,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price, :image)
