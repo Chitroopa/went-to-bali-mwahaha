eb98afb43bcee205293dde876d088c47877fc3f2
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-07-14 11:22:40 -0700

    Add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_many :order_items
+  validates :name, :description, :price, presence: true
+  validates_numericality_of :price, greater_than: 0
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   post '/sign_in' => 'sessions#create'
   get 'sign_out' => 'sessions#destroy'
 
+  get '/admin' => 'users#admin'
+
   resources :products
   resources :order_items
   resource :cart, only: [:show]
